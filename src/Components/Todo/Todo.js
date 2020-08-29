9a23dbc703bb17c42522a6972bacb1888ff6b067
Shakib448 <shakiba448@gmail.com>
2020-08-29 15:57:14 +0600

    React Todo App

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -5,13 +5,24 @@ const Todo = ({text ,setTodos, todos, todo}) => {
     const deleteHandler = () => {
         setTodos(todos.filter((el) => el.id !== todo.id))
     };
+
+    const completeHandler = () => {
+        setTodos(todos.map((item) => {
+            if(item.id === todo.id){
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }))
+    };
      
     return (
         <>
 
         <div className="todo">
-            <li className="todo-item">{text}</li>
-            <button className="complete-btn"><i className="fas fa-check"></i></button>
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+            <button onClick={completeHandler} className="complete-btn"><i className="fas fa-check"></i></button>
             <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
 
